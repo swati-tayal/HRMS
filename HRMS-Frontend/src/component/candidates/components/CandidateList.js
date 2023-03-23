17ac0bc7d078c8a9768ca472bfc9c2689f3378e2
swati tayal <swati.tayal@magicedtech.com>
2023-03-23 20:39:22 +0530

    restructuring

--- HRMS-Frontend/src/component/candidates/components/CandidateList.js
+++ HRMS-Frontend/src/component/candidates/components/CandidateList.js
@@ -1,33 +1,31 @@
 import React from "react";
 
-import InterviewerItem from "./InterviewerItem";
-import Card from "../../shared/components/UIElements/Card";
-import "./InterviewersList.css";
+import CandidateItem from "./CandidateItem";
+import Card from "../../../shared/components/UIElements/Card";
+import "./CandidateList.css";
 
-const InterviewersList = (props) => {
-
+const CandidateList = (props) => {
     if (props.items.length === 0) {
         return (
           <div className="center">
             <Card>
-              <h2>No Interviewers found.</h2>
+              <h2>No Candidates found.</h2>
             </Card>
           </div>
         );
       }
     
       return (
-        <ul className="interviewrs-list">
+        <ul className="candidates-list">
           {props.items.map((user) => (
-            <InterviewerItem
+            <CandidateItem
               key={user.id}
               id={user.id}
               name={user.name}
-              interviewCount={user.interviews.length}
             />
           ))}
         </ul>
       );
 }
 
-export default InterviewersList;
+export default CandidateList;
